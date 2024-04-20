37e63777a8449524206c8c0c2ef38222ba078309
Fabian Grosch <fabian.grosch@iis.fraunhofer.de>
2024-04-20 14:56:00 +0200

    fix: remove cross origin annotation from ExamRepository

--- classplanner.api/src/main/java/de/dev101/classplanner/api/endpoints/exams/ExamRepository.java
+++ classplanner.api/src/main/java/de/dev101/classplanner/api/endpoints/exams/ExamRepository.java
@@ -1,8 +1,6 @@
 package de.dev101.classplanner.api.endpoints.exams;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
 public interface ExamRepository extends MongoRepository<Exam, String> {
 }
