ce83647df5906e24f9215c9a307abda0d9403519
Fabian Grosch <fabian.grosch@iis.fraunhofer.de>
2024-04-20 14:56:25 +0200

    fix: remove cross origin annotation from StudentRepository

--- classplanner.api/src/main/java/de/dev101/classplanner/api/endpoints/students/StudentRepository.java
+++ classplanner.api/src/main/java/de/dev101/classplanner/api/endpoints/students/StudentRepository.java
@@ -1,8 +1,6 @@
 package de.dev101.classplanner.api.endpoints.students;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
 public interface StudentRepository extends MongoRepository<Student, String> {
 }
