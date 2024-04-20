7494ff6f8c46dfecd40f7ee8bb0b5caa0a5b9f05
Fabian Grosch <fabian.grosch@iis.fraunhofer.de>
2024-04-20 14:56:52 +0200

    feat: add missing CRUD operations for Student entity

--- classplanner.api/src/main/java/de/dev101/classplanner/api/endpoints/students/StudentController.java
+++ classplanner.api/src/main/java/de/dev101/classplanner/api/endpoints/students/StudentController.java
@@ -3,7 +3,9 @@ package de.dev101.classplanner.api.endpoints.students;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,25 +19,37 @@ public class StudentController {
 
     private final StudentRepository studentRepository;
 
-    public StudentController(StudentRepository studentRepository) {
+    StudentController(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
+    @GetMapping("/{id}")
+    Student getStudent(@PathVariable String id) {
+        return studentRepository
+                .findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Student not found with id " + id));
+    }
+
     @GetMapping
-    public List<Student> getAllStudents() {
+    List<Student> getAllStudents() {
         return studentRepository.findAll();
     }
 
     @PostMapping
-    public Student createStudent(@RequestBody Student student) {
+    Student createStudent(@RequestBody Student student) {
         return studentRepository.save(student);
     }
 
-    @PutMapping
-    public Student updateStudent(@RequestBody Student student) {
+    @PutMapping("/{id}")
+    Student updateStudent(@PathVariable String id, @RequestBody Student student) {
         var studentId = student.id();
         return studentRepository.findById(studentId)
                 .map(s -> studentRepository.save(student))
                 .orElseThrow(() -> new IllegalArgumentException("Student not found with id " + studentId));
+    }
+
+    @DeleteMapping("/{id}")
+    void deleteStudent(@PathVariable String id) {
+        studentRepository.deleteById(id);
     }
 }
